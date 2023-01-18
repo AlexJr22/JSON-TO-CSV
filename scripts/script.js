e6ea32a1c9f017c861d89188a99b39bb5234f953
Alex Junior <alexjr2oficial@gmail.com>
2023-01-17 21:10:17 -0300

    chamando a funcÃção que valida se o input  está em um formato json

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 const inputJson = document.getElementById("inputText"); // recebe o valor de um textarea
+const btnConverter = document.getElementById("btnConverter"); // botão que chama a função que converte os valores
 
 const validarJson = (json) => {
   let StringJson = String(json); // converte o valor do inputJson para uma string
@@ -11,3 +12,7 @@ const validarJson = (json) => {
     alert("Erro: O valor passado não está em formato Json!");
   }
 };
+
+btnConverter.addEventListener("click", () => {
+  validarJson(inputJson.value);
+});
