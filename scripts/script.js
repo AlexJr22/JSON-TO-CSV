deaa0a20bcc0e246b67bba6f163ae8af849852bc
Alex Junior <alexjr2oficial@gmail.com>
2023-01-18 21:09:51 -0300

    finalizando a função converterJson

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,19 @@
 const inputJson = document.getElementById("inputText"); // recebe o valor de um textarea
 const btnConverter = document.getElementById("btnConverter"); // botão que chama a função que converte os valores
+const outputCsv = document.getElementById("outputCsv");
+
+const converterJson = (json) => {
+  const keys = Object.keys(json[0]); // captura as keys dos elementos
+
+  console.log(keys); // mostra no console as keys
+  outputCsv.innerHTML = `${keys}<br>`;
+
+  json.forEach((element, index) => {
+    const values = Object.values(json[index]);
+    console.log(values);
+    outputCsv.innerHTML += `${values}<br>`;
+  });
+};
 
 const validarJson = (json) => {
   let StringJson = String(json.value); // converte o valor do inputJson para uma string
@@ -11,17 +25,6 @@ const validarJson = (json) => {
     console.log(error);
     alert("Erro: O valor passado não está em formato Json!");
   }
-};
-
-const converterJson = (json) => {
-  const keys = Object.keys(json[0]); // captura as keys dos elementos
-
-  console.log(keys); // mostra no console as keys
-
-  json.forEach((element, index) => {
-    const values = Object.values(json[index]);
-    console.log(values);
-  });
 };
 
 btnConverter.addEventListener("click", () => {
